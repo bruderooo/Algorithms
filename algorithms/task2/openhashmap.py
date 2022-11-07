1bfd622c365ac71ac383553ad26763b839a502eb
bruderooo <szymonjacon@gmail.com>
2022-11-07 19:04:46 +0100

    **fix** open hash map (now use DELETE type)

--- algorithms/task2/openhashmap.py
+++ algorithms/task2/openhashmap.py
@@ -1,10 +1,21 @@
+from dataclasses import dataclass
 from typing import Any, Generator, Hashable
 
 from algorithms.task2.map import Map
 from algorithms.task2.utils import hash_sized, next_pow_2
 
 
+class Deleted:
+    pass
+
+
+def check_element(item: Any) -> bool:
+    return item is not None and item is not OpenHashMap.DELETED
+
+
 class OpenHashMap(Map):
+    DELETED: Deleted = Deleted()
+
     def __setitem__(self, key, value) -> None:
         self.insert(key, value)
 
@@ -14,6 +25,7 @@ class OpenHashMap(Map):
 
             if (
                 (element := self._key_val_table[key_hash]) is None
+                or element is OpenHashMap.DELETED
                 or (element[0] == key and type(element[0]) == type(key))
             ):
                 self._key_val_table[key_hash] = (key, value)
@@ -24,14 +36,11 @@ class OpenHashMap(Map):
         for i in range(self._size):
             key_hash = hash_sized(key, self._size, i)
 
-            if (
-                (element := self._key_val_table[key_hash]) is not None
-                and element[0] == key
-                and type(element[0]) == type(key)
-            ):
-                return element[1]
-
-        raise KeyError(key)
+            if (element := self._key_val_table[key_hash]) is not None:
+                if element is not OpenHashMap.DELETED and element[0] == key and type(element[0]) == type(key):
+                    return element[1]
+            else:
+                raise KeyError(key)
 
     def __delitem__(self, key: Hashable):
         for i in range(self._size):
@@ -39,10 +48,11 @@ class OpenHashMap(Map):
 
             if (
                 (element := self._key_val_table[key_hash]) is not None
+                and element is not OpenHashMap.DELETED
                 and element[0] == key
                 and type(element[0]) == type(key)
             ):
-                self._key_val_table[key_hash] = None
+                self._key_val_table[key_hash] = OpenHashMap.DELETED
                 self._fix_map_size()
                 return
 
@@ -65,9 +75,8 @@ class OpenHashMap(Map):
             for i in range(size):
                 key_hash = hash_sized(key, size, i)
 
-                if (
-                    (element := new_key_val_table[key_hash]) is None
-                    or (element[0] == key and type(element[0]) == type(key))
+                if (element := new_key_val_table[key_hash]) is None or (
+                    element[0] == key and type(element[0]) == type(key)
                 ):
                     new_key_val_table[key_hash] = (key, value)
                     break
@@ -76,13 +85,13 @@ class OpenHashMap(Map):
         self._key_val_table = new_key_val_table
 
     def items(self) -> filter:
-        return filter(lambda item: item is not None, self._key_val_table)
+        return filter(check_element, self._key_val_table)
 
     def keys(self) -> Generator[Hashable, None, None]:
-        return (item[0] for item in self._key_val_table if item is not None)
+        return (item[0] for item in self._key_val_table if check_element(item))
 
     def values(self) -> Generator[Any, None, None]:
-        return (item[1] for item in self._key_val_table if item is not None)
+        return (item[1] for item in self._key_val_table if check_element(item))
 
     def __len__(self) -> int:
-        return sum(1 for el in self._key_val_table if el is not None)
+        return sum(1 for item in self._key_val_table if check_element(item))
