daaad65d6f78161c1b6d4cbe0445ad59bb0575e6
bruderooo <szymonjacon@gmail.com>
2022-12-20 19:47:19 +0100

    **add** lcs version for lower memory

--- algorithms/task3/longest_subsequence.py
+++ algorithms/task3/longest_subsequence.py
@@ -31,3 +31,35 @@ def lcs_with_cached_table(seq1, seq2):
 
     _lcs(len(seq1) - 1, len(seq2) - 1)
     return cached_table[(len(seq1) - 1, len(seq2) - 1)]
+
+
+def lcs_lower_memory(seq1, seq2):
+    i = len(seq1) - 1
+    j = len(seq2) - 1
+    cached_table = {}
+    out = []
+
+    def _lcs(i: int, j: int) -> int:
+        if (i, j) not in cached_table:
+            if i == -1 or j == -1:
+                cached_table[(i, j)] = 0
+            elif seq1[i] == seq2[j]:
+                cached_table[(i, j)] = _lcs(i - 1, j - 1) + 1
+            else:
+                cached_table[(i, j)] = max(_lcs(i - 1, j), _lcs(i, j - 1))
+
+        return cached_table[(i, j)]
+
+    _lcs(i, j)
+
+    while i > -1 and j > -1:
+        if seq1[i] == seq2[j]:
+            out += [seq1[i]]
+            i -= 1
+            j -= 1
+        elif cached_table[(i - 1, j)] > cached_table[(i, j - 1)]:
+            i -= 1
+        else:
+            j -= 1
+
+    return out[::-1]
